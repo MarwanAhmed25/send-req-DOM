58c04ec37c40a412c1e8d6e5fda9a5263fed9904
MarwanAhmed25 <marwan4125881@gmail.com>
2022-04-22 13:23:25 +0200

    first

---
+++ app.js
@@ -0,0 +1,123 @@
+//import get_random_cook from "./cooki";
+
+const ids = [
+  "nat_nam",
+  "f_name",
+  "l_name",
+  "father_name",
+  "mother_name",
+  "mobile_num",
+  "applied_kind",
+  "center",
+  "nat_num_ext",
+  "cop_num",
+];
+var arr_of_ids_result = [];
+count = 0; 
+
+class Table {
+  row;
+
+  constructor() {
+    this.row = document.createElement("TR");
+  }
+
+  clicked_button(e) {
+    for (var i = 0; i < ids.length; i++) {
+      var value = document.getElementById(ids[i]).value;
+        if(!value)
+            return false;
+      var td = document.createElement("TD");
+      td.innerText = value;
+      this.row.appendChild(td);
+    }
+
+    var td = document.createElement("TD");
+    td.innerText = 0;
+    td.setAttribute('id',`num_of_req${count}`);
+    count +=1;
+    this.row.appendChild(td);
+    
+    document.getElementById("results").appendChild(this.row);
+        this.run();
+    this.clear__();
+    return true;
+  }
+
+  run(){
+    setInterval(fetch_data,1000); 
+  }
+
+  clear__() {
+    for (var i = 0; i < ids.length; i++) {
+      document.getElementById(ids[i]).value = "";
+    }
+  }
+}
+
+document.getElementById("submit").addEventListener("click", (e) => {
+  let t = new Table();
+  arr_of_ids_result.push(t);
+  var b = t.clicked_button(e);
+  if(b){
+    for(let i=0; i< arr_of_ids_result.length; i++){
+          arr_of_ids_result[i].run();
+    }
+  }
+});
+
+document.getElementById("clear").addEventListener("click", (e) => {
+  get_random_cook();
+  let t = new Table();
+  t.clear__(e);
+});
+
+
+function fetch_data(){
+
+    d = document.getElementById(`num_of_req${count-1}`);
+    d.innerText = 1 + Number(d.innerText);
+
+}
+
+
+var cook = [
+  { cook_info: { PHPSESSID: "6mjrhtj878784di0db1d0uttlj", captcha: "033881" } },
+  { cook_info: { PHPSESSID: "udhvfe817clov6so09og7nfhi4", captcha: "450619" } },
+  { cook_info: { PHPSESSID: "e1t3lj3nu8dcv1ah59ltdrjqp8", captcha: "150544" } },
+  { cook_info: { PHPSESSID: "gbvf918l092qnvliai6lo1d9p3", captcha: "484112" } },
+  { cook_info: { PHPSESSID: "8rklih1kebsni9h3asm43cfe7s", captcha: "443832" } },
+  { cook_info: { PHPSESSID: "hm13s6a3gijnjs67f005v4o49t", captcha: "092951" } },
+  { cook_info: { PHPSESSID: "f0di4bjgifgcrck3tk2eg9tu2j", captcha: "597004" } },
+  { cook_info: { PHPSESSID: "2t0d0hh5fiktdmrk170vh84db7", captcha: "554820" } },
+  { cook_info: { PHPSESSID: "h5h0emrvd3q3fccacf6r4uhcuq", captcha: "739164" } },
+  { cook_info: { PHPSESSID: "fp3ievrceiultuak6ebsmr95ct", captcha: "150613" } },
+  { cook_info: { PHPSESSID: "tbi53rlqlsvqjdu2t646i9pjmv", captcha: "276596" } },
+  { cook_info: { PHPSESSID: "6j4bruid51fbj31l02fomka7uu", captcha: "654236" } },
+  { cook_info: { PHPSESSID: "tpvo4ivl6e6t7uikeqrm8k929k", captcha: "927091" } },
+  { cook_info: { PHPSESSID: "q49g0tqqoqivjvj5k4eg3co9t3", captcha: "811287" } },
+  { cook_info: { PHPSESSID: "svib203bvkic6fnsonh9tulqiv", captcha: "278701" } },
+  { cook_info: { PHPSESSID: "2n0c0ih0u10jdsqr6p0atsc30i", captcha: "708170" } },
+  { cook_info: { PHPSESSID: "75h5nco2pjlj3t6bjhk52ahalc", captcha: "020818" } },
+  { cook_info: { PHPSESSID: "1gon3j8prtqihb83nq8ohpb1hb", captcha: "228449" } },
+  { cook_info: { PHPSESSID: "ihafih2llldispl2984i3s7vba", captcha: "687616" } },
+  { cook_info: { PHPSESSID: "5j9dh8cb9nvr24otg1lf6mrqnt", captcha: "649649" } },
+  { cook_info: { PHPSESSID: "vedoshq5sbt01gaslog58eovsj", captcha: "038021" } },
+  { cook_info: { PHPSESSID: "kprok9lgo7dv3rmna47g0vcgm6", captcha: "412155" } },
+  { cook_info: { PHPSESSID: "2f7rl6umcm5c67r3ir4o43rna3", captcha: "268402" } },
+  { cook_info: { PHPSESSID: "mdcmnkslk3kth6avp871oe9n28", captcha: "430709" } },
+  { cook_info: { PHPSESSID: "qvmip95v0llhctq7ulndue1e5p", captcha: "611523" } },
+  { cook_info: { PHPSESSID: "9s993lqblkjoo1pvtb3d2rvc4v", captcha: "731574" } },
+  { cook_info: { PHPSESSID: "1t02e0v1i7lufa10o33sm0ihn5", captcha: "266384" } },
+  { cook_info: { PHPSESSID: "1nap0q36dbtlubqgdi4t8q2v0p", captcha: "400360" } },
+  { cook_info: { PHPSESSID: "lp80iebcgn5ngjkqoat1qcl90m", captcha: "038935" } },
+];
+
+function random_num() {
+  return Math.floor(Math.random() * 100) % 29;
+}
+
+function get_random_cook() {
+  var n = random_num();
+  return cook[n].cook_info;
+}
